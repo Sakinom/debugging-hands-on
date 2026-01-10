6ba1ebe9c577c8ac2b9b615362c054ace7dc4f17
Sakinom <sakinom328@gmail.com>
2026-01-10 15:15:36 +0900

    feat: Sandpackの環境構築

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import { Sandpack } from '@codesandbox/sandpack-react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Sandpack template="react" />
+    </div>
+  )
+}
+
+export default App
