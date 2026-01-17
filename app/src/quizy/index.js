e877e6539afea4be7ca0966dc09fec1fa69009c2
Sakinom <sakinom328@gmail.com>
2026-01-17 11:19:10 +0900

    refactor: HTML/CSS/JSの整理とクイズデータに画像プロパティを追加

--- app/src/quizy/index.js
+++ app/src/quizy/index.js
@@ -7,6 +7,7 @@
    * @property {string | undefined} note ノート
    * @property {string} question 問題文
    * @property {string[]} answers 回答の配列
+   * @property {string} image 画像ファイル名
    */
 
   /**
@@ -19,39 +20,45 @@
       question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
       answers: ['約28万人', '約79万人', '約183万人'],
       correctNumber: 1,
-      note: '経済産業省 2019年3月 － IT 人材需給に関する調査'
+      note: '経済産業省 2019年3月 － IT 人材需給に関する調査',
+      image: 'it-jinzai.png'
     },
     {
       id: 2,
       question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
       answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
       correctNumber: 2,
+      image: 'x-tech.png'
     },
     {
       id: 3,
       question: 'IoTとは何の略でしょう？',
       answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
       correctNumber: 0,
+      image: 'iot.png'
     },
     {
       id: 4,
       question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
       answers: ['Society 5.0', 'CyPhy', 'SDGs'],
       correctNumber: 0,
-      note: 'Society5.0 - 科学技術政策 - 内閣府'
+      note: 'Society5.0 - 科学技術政策 - 内閣府',
+      image: 'society5.0.png'
     },
     {
       id: 5,
       question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
       answers: ['Web3.0', 'NFT', 'メタバース'],
       correctNumber: 0,
+      image: 'web3.0.png'
     },
     {
       id: 6,
       question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
       answers: ['約2倍', '約5倍', '約11倍'],
       correctNumber: 1,
-      note: 'Accenture Technology Vision 2021'
+      note: 'Accenture Technology Vision 2021',
+      image: 'tech-usage.png'
     }
   ];
 
@@ -93,7 +100,7 @@
             class="p-quiz-box__question__title__text">${quizItem.question}</span>
         </h2>
         <figure class="p-quiz-box__question__image">
-          <img src="https://github.com/Sakinom/debugging-hands-on/blob/main/app/src/assets/img/quiz/img-quiz0${quizItem.id}.png" alt="">
+          <p>assets/img/quiz/${quizItem.image}</p>
         </figure>
       </div>
       <div class="p-quiz-box__answer">
